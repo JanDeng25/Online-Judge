bd621a09943e125e8489fb5f578c03f8d7761f29
Jan Deng <1003932334@qq.com>
2016-05-24 22:21:59 +0800

    Day 23

---
+++ leetcode OJ/204. Count Primes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        //∂Ú¿≠∂‡»˚…∏∑® Sieve of Eeatosthese
+        bool *isPrime = new bool[n];
+        for(int i = 0 ; i < n; i ++)
+            isPrime[i] = true;
+        for(int i = 2; i * i < n; i ++){
+            if(!isPrime[i])  continue;
+            
+                for(int j = i * i; j < n; j += i){
+                    isPrime[j] = false;
+                }
+            
+        }
+        int count = 0;
+        for(int  i = 2 ;i < n; i ++){
+            if(isPrime[i])
+                count ++;
+        }
+        
+        return count;
+    }
+};
