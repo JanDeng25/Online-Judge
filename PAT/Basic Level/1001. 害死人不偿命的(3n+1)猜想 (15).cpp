8e4355f14872a32ff8f16809d93eaca232b98ffb
Jan Deng <1003932334@qq.com>
2016-05-29 21:38:19 +0800

    May 29, 2016

---
+++ PAT/Basic Level/1001. 害死人不偿命的(3n+1)猜想 (15).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int step = 0;
+	cin >> n;
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n /= 2;
+			step ++;
+		}
+		else {
+			n = (3 * n + 1) / 2;
+			step ++;
+		}
+	}
+	cout << step << endl;
+	return 0;
+}
