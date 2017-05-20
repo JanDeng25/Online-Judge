c05a0ea20ef46c41bf8805c8d02e15329dab9feb
Jan Deng <1003932334@qq.com>
2017-05-20 14:23:58 +0800

    get sum of every 2-tuples

---
+++ leetcode OJ/561. Array Partition I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        //找出所有两个一组中较小的那个数的和
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i += 2){
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
